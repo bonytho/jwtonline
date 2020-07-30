c327364b81be431a2e4468e52fb8c2c84c63b562
Bony Thomas <boneyt92@gmail.com>
2020-07-30 15:57:57 +0530

    Add: JWT Decode component

---
+++ src/app/jwtdecode/jwtdecode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jwtdecode',
+  templateUrl: './jwtdecode.component.html',
+  styleUrls: ['./jwtdecode.component.css']
+})
+export class JwtdecodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
