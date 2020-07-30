5c8a611a03d93b51e1346bd1783a35ee1d580c81
Bony Thomas <boneyt92@gmail.com>
2020-07-30 22:33:38 +0530

    Add: Decode JWT

--- src/app/jwtdecode/jwtdecode.component.ts
+++ src/app/jwtdecode/jwtdecode.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-jwtdecode',
   templateUrl: './jwtdecode.component.html',
-  styleUrls: ['./jwtdecode.component.css']
+  styleUrls: ['./jwtdecode.component.css'],
 })
-export class JwtdecodeComponent implements OnInit {
+export class JwtdecodeComponent implements AfterViewInit, OnInit {
+  @ViewChild('jwtToken') jwtTokenRaw: ElementRef;
 
-  constructor() { }
+  jwtToken: string;
+  decodedData: Array<any> = [];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.jwtTokenRaw.nativeElement.innerHTML = this.jwtToken;
   }
 
+  tokenChanged(): void {
+    console.log('this.tokenChanged');
+    let isChanged = false;
+    const jwtToken = this.jwtTokenRaw.nativeElement.innerHTML;
+    if (jwtToken !== this.jwtToken) {
+      this.jwtToken = jwtToken;
+      isChanged = true;
+    }
+    if (this.jwtToken !== '') {
+      this.decodeToken();
+    }
+  }
+
+  formatTokenRaw(): void {}
+
+  decodeToken(): void {
+    try {
+      const splitted = this.jwtToken.split('.');
+      if (splitted.length !== 3) {
+        throw new Error('Invalid JWT');
+      }
+      this.formatTokenRaw();
+      for (let i = 0; i < 2; i++) {
+        this.decodedData[i] = JSON.stringify(
+          JSON.parse(window.atob(splitted[i])),
+          undefined,
+          2
+        );
+      }
+      console.log(this.decodedData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 }
